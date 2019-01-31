a278aa8dae7799496240e91be275c89b3a49c84d
Fritz Meyer <meyerfr@gmx.de>
2019-01-31 18:12:32 +0100

    testing mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  after_creater :send_welcome_email
   has_many :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
