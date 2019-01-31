8b94f3a6820fbfc30f03898de9f094f8fdb2dcb6
Fritz Meyer <meyerfr@gmx.de>
2019-01-31 17:59:46 +0100

    Added mailer in development and production

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable, :invitable, invite_for: 3.days
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
+  end
 end
