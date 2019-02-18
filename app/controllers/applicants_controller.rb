34d7e17417972918dc3787a6165fe6848b4314de
Fritz Meyer <meyerfr@gmx.de>
2019-02-18 12:07:31 +0100

    Added Delete Method to Applicants

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -23,6 +23,12 @@ class ApplicantsController < ApplicationController
     end
   end
 
+  def destroy
+    @applicant = Applicant.find(params[:id])
+    @applicant.destroy
+    redirect_to applicants_path
+  end
+
   private
 
   def applicants_params
