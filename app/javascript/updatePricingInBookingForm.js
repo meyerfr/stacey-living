5cdb6b37d58b48e1accde1fcbdce7d5f74b3f9ca
Fritz Meyer <meyerfr@gmx.de>
2019-04-28 12:17:56 +0200

    updated booking-form-pricing deposit and color when change

--- app/javascript/updatePricingInBookingForm.js
+++ app/javascript/updatePricingInBookingForm.js
@@ -3,18 +3,22 @@ const updatePricing = (event) => {
   var price = document.querySelector('.price-per-month');
   var prices = document.getElementById('prices').children;
   var deposit = document.querySelector('.deposit-text');
+  var info_fields = document.querySelectorAll('.info-field');
 
   // newDuration = The new duration of Stay
   if (parseInt(newDuration, 10) < 5) {
     price.innerHTML = `€ ${prices[0].innerHTML}`
-    deposit.innerHTML = `€ ${prices[0].innerHTML}`
+    deposit.innerHTML = `1 monthly rent`
   } else if (parseInt(newDuration, 10) < 8) {
     price.innerHTML = `€ ${prices[1].innerHTML}`
-    deposit.innerHTML = `€ ${parseInt(prices[0].innerHTML) * 2}`
+    deposit.innerHTML = `2 monthy rents`
   } else {
     price.innerHTML = `€ ${prices[2].innerHTML}`
-    deposit.innerHTML = `€ ${parseInt(prices[0].innerHTML) * 3}`
+    deposit.innerHTML = `3 monthy rents`
   };
+  info_fields.forEach((field) => {
+    field.classList.add('changed');
+  });
 }
 
 function addEventListenerToDuration() {
