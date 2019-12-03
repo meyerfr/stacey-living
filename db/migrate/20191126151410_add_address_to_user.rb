8f81a801fb3004ca3e776066a3260343ef763550
Fritz Meyer <meyerfr@gmx.de>
2019-12-03 13:20:51 +0100

    created slider and fixed house_number bug

--- db/migrate/20191126151410_add_address_to_user.rb
+++ db/migrate/20191126151410_add_address_to_user.rb
@@ -1,5 +1,8 @@
 class AddAddressToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :address, :string
+    add_column :users, :street, :string
+    add_column :users, :city, :string
+    add_column :users, :zipcode, :integer
+    add_column :users, :country, :string
   end
 end
