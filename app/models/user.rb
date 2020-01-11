f355593bde2e32b96481b4f59a16dd98b2287ac5
Fritz Meyer <meyerfr@gmx.de>
2020-01-11 16:48:20 +0100

    reinitialized user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   attr_accessor :skip_password_validation, :name, :bookings_attributes # virtual attribute to skip password validation while saving
-  # validate :validate_array
-  # validate :move_in_future
+  validate :validate_array
+  validate :move_in_future
   # validate :stay_duration
   # validate :validate_prefered_suite
   # validate :validate_gender
