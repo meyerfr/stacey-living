458326c729acff8dc6e4818281e8ecfb266082dc
Fritz Meyer <meyerfr@gmx.de>
2020-02-17 17:17:17 +0100

    added stripe logic to payment controller. And iserted stripeProcess.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 import "bootstrap";
+import { stripeProcess } from '../stripePayment'
 import { onlyOneBox } from '../onlyOneBox';
 import { socialMediaForms } from '../applySocialMediaForms';
 import { expandConfirm } from '../welcomeCallCalendar';
@@ -10,6 +11,7 @@ import { infoOnHovering } from '../hover_effect_for_info'
 import { checkBookingFormDuration } from '../bookingFormCheck'
 import { signing } from '../signingProcess'
 
+stripeProcess();
 onlyOneBox();
 socialMediaForms();
 expandConfirm();
