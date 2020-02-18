d0e603ea3bc1241aedeb52d3421261e2e42c46d0
Fritz Meyer <meyerfr@gmx.de>
2020-02-18 09:15:46 +0100

    added every estential part for stripe, but still not working yet

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -10,6 +10,7 @@ class PaymentsController < ApplicationController
   def new
     # layout booking
     @phone_code = %w(+61 +43 +32 +55 +1 +86 +45 +358 +33 +49 +852 +353 +39 +81 +352 +52 +31 +64 +47 +351 +65 +34 +46 +41 +44)
+    #@list = Stripe::Customer.list()
   end
 
   def create
