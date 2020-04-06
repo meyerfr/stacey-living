a93ad73a87934ae45d119bd7eee28f81c447de1a
Fritz Meyer <meyerfr@gmx.de>
2020-04-06 08:32:52 +0200

    fixed error in WelcomeCallJob

--- app/jobs/welcome_call_job.rb
+++ app/jobs/welcome_call_job.rb
@@ -4,7 +4,7 @@ class WelcomeCallJob < ApplicationJob
   def perform
     WelcomeCall.select{|c| c.start_time.to_date < Date.today && c.available == true }.each{|c| c.delete }
     if Date.today.wday > 0 && Date.today.wday < 6
-      WelcomeCall.create(start_time: Time.parse("#{Date.today + 14} 10:00"), end_time: Time.parse("#{Date.today + 7} 10:15"))
+      WelcomeCall.create(start_time: Time.parse("#{Date.today + 14} 10:00"), end_time: Time.parse("#{Date.today + 14} 10:15"))
       while WelcomeCall.last.start_time < Time.parse("#{Date.today + 14} 17:30")
         WelcomeCall.create(start_time: WelcomeCall.last.start_time + 30.minutes, end_time: WelcomeCall.last.end_time + 30.minutes)
       end
