af64b8c1e5bbccc1769166fb512c0c1cb02c2524
Fritz Meyer <meyerfr@gmx.de>
2020-04-30 14:35:53 +0200

    updated user and booking validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,36 @@
 class Booking < ApplicationRecord
+  validate :move_in_future, :minimum_duration
   belongs_to :user
   belongs_to :room_attribute, optional: true
   has_many :contracts, dependent: :destroy
   has_many :welcome_calls, dependent: :destroy
+
+  # Validate whether the start time is in the future
+  def move_in_future
+    if move_in.present? && move_in < Date.today
+      errors.add(:move_in, "can´t be in the past.")
+    end
+  end
+
+  # Validate whether the end_time is at least 3 hrs after the start_time
+  def minimum_duration
+    if move_in.present? && move_out.present? && move_out < (move_in.advance(:months => 3))
+      errors.add(:move_out, "3 Months minimum")
+    end
+  end
+
+  # Validate whether the availability is not overlapping any already existing availabilities
+  # def booking_overlap
+  #   if start_time.present? && end_time.present?
+  #     Availability.where(sitter: sitter).each do |availability|
+  #       if start_time > availability.start_time.advance(:hours => -0.5) && start_time < availability.end_time.advance(:hours => +0.5)
+  #         errors.add(:start_time, "is within 30 minutes of or during another one of your availabilities.")
+  #         break
+  #       elsif end_time > availability.start_time.advance(:hours => -0.5) && end_time < availability.end_time.advance(:hours => +0.5)
+  #         errors.add(:end_time, "is within 30 minutes of or during another one of your availabilities.")
+  #         break
+  #       end
+  #     end
+  #   end
+  # end
 end
