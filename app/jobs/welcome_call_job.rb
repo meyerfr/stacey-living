541db390f3df7129f356a4dbc7143fbe16adb9b1
Fritz Meyer <meyerfr@gmx.de>
2020-08-27 09:25:48 +0200

    commented WelcomeCallJob

--- app/jobs/welcome_call_job.rb
+++ app/jobs/welcome_call_job.rb
@@ -1,16 +1,16 @@
 class WelcomeCallJob < ApplicationJob
-  queue_as :default
+  # queue_as :default
 
-  def perform
-    todays_date = Date.today
-    WelcomeCall.where("start_time < ? AND available = ?", todays_date, true).destroy_all
-    if todays_date.wday < 6 && todays_date.wday > 0
-      WelcomeCall.create(start_time: Time.parse("#{todays_date + 14} 10:00"), end_time: Time.parse("#{todays_date + 14} 10:15"))
-      while WelcomeCall.last.start_time < Time.parse("#{todays_date + 14} 17:30")
-        WelcomeCall.create(start_time: WelcomeCall.last.start_time + 30.minutes, end_time: WelcomeCall.last.end_time + 30.minutes)
-      end
-    end
-  end
+  # def perform
+  #   todays_date = Date.today
+  #   WelcomeCall.where("start_time < ? AND available = ?", todays_date, true).destroy_all
+  #   if todays_date.wday < 6 && todays_date.wday > 0
+  #     WelcomeCall.create(start_time: Time.parse("#{todays_date + 14} 10:00"), end_time: Time.parse("#{todays_date + 14} 10:15"))
+  #     while WelcomeCall.last.start_time < Time.parse("#{todays_date + 14} 17:30")
+  #       WelcomeCall.create(start_time: WelcomeCall.last.start_time + 30.minutes, end_time: WelcomeCall.last.end_time + 30.minutes)
+  #     end
+  #   end
+  # end
 
   # def reschedule_job
   #   self.class.set(wait: 24.hours).perform_later
